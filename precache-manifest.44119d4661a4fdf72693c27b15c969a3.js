981a6f4eee20f62ebf976456752a12aa7493ed28
Parad0xJ <paradoxlyon@gmail.com>
2020-02-24 13:38:21 +0100

    Updates

---
+++ precache-manifest.44119d4661a4fdf72693c27b15c969a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8538e6b295449c4c339e89ea6073d90d",
+    "url": "/todosapprrd/index.html"
+  },
+  {
+    "revision": "e82d5d8c9a6313eac34e",
+    "url": "/todosapprrd/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a1d4fc79cb680a224f54",
+    "url": "/todosapprrd/static/css/main.64e9dbcb.chunk.css"
+  },
+  {
+    "revision": "e82d5d8c9a6313eac34e",
+    "url": "/todosapprrd/static/js/2.048ebb49.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todosapprrd/static/js/2.048ebb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d4fc79cb680a224f54",
+    "url": "/todosapprrd/static/js/main.02689c28.chunk.js"
+  },
+  {
+    "revision": "76ccf763c97b66a9c5ed",
+    "url": "/todosapprrd/static/js/runtime-main.42391120.js"
+  }
+]);
